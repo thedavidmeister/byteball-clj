52bbbf9f1d4f6ca4fbbab5639478f9d9e1c44c33
David Meister <thedavidmeister@gmail.com>
2018-02-17 12:55:11 +1100

    WIP on source string tests

--- build.boot
+++ build.boot
@@ -11,12 +11,14 @@
    [cheshire "5.8.0"]
    [samestep/boot-refresh "0.1.0"]
    [pandect "0.6.1"]
+   [adzerk/boot-test "RELEASE" :scope "test"]
    [com.github.Sepia-Officinalis/secp256k1 "fd44e1e0d6"]]
  :repositories
  #(conj % '["jitpack" "https://jitpack.io"]))
 
 (require
- '[samestep.boot-refresh :refer [refresh]])
+ '[samestep.boot-refresh :refer [refresh]]
+ '[adzerk.boot-test :refer [test]])
 
 (deftask repl-server
  []
--- src/serialize/source_string.clj
+++ src/serialize/source_string.clj
@@ -1,6 +1,10 @@
-(ns serialize.source-string)
+(ns serialize.source-string
+ (:require
+  [clojure.test :refer [deftest is]]))
 
 (def string-join-char "\u0000")
+
+(def source-seq->source-string (partial clojure.string/join string-join-char))
 
 ; https://github.com/byteball/byteballcore/blob/master/string_utils.js#L11
 (defprotocol ISourceString
@@ -8,13 +12,13 @@
 
 (extend-protocol ISourceString
  java.lang.String
- (->source-string [this] (str "s" this))
+ (->source-string [this] (source-seq->source-string ["s" this]))
 
  java.lang.Long
- (->source-string [this] (str "n" this))
+ (->source-string [this] (source-seq->source-string ["n" this]))
 
  java.lang.Boolean
- (->source-string [this] (str "b" this))
+ (->source-string [this] (source-seq->source-string ["b" this]))
 
  clojure.lang.PersistentVector
  (->source-string [this]
@@ -36,3 +40,26 @@
 
  clojure.lang.Keyword
  (->source-string [this] (->source-string (name this))))
+
+; TESTS
+
+; mirroring byteballcore fns
+(deftest ??simple-string
+ (is
+  (=
+   (source-seq->source-string ["s" "simple test string"])
+   (->source-string "simple test string")))
+
+ (is (= 20 (count (->source-string "simple test string")))))
+
+(deftest ??integer
+ (is
+  (=
+   (source-seq->source-string ["n" 27090])
+   (->source-string 27090))))
+
+(deftest ??boolean
+ (is
+  (=
+   (source-seq->source-string ["b" false])
+   (->source-string false))))
