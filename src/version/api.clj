ea989ee45a0ac450fe8c16cb54d4cf56de153c53
David Meister <thedavidmeister@gmail.com>
2018-02-22 02:46:44 +1100

    old-core?

--- src/version/api.clj
+++ src/version/api.clj
@@ -11,3 +11,22 @@
   (str "Incompatible " s ", mine " mine ", yours " yours))
  (taoensso.timbre/error "incompatible " s)
  (manifold.stream/close! conn))
+
+(defn version->int
+ [version]
+ (let [[major minor patch] (clojure.string/split version ".")]
+  (int
+   (+
+    (* major 10000)
+    (* minor 100)
+    patch))))
+
+(defn old-core?
+ [version-map]
+ (let [{:keys [library-version]} version-map]
+  (and
+   (string? library-version)
+   (<
+    (version->int library-version)
+    (version->int "0.2.70"))
+   (= "1.0" version.data/protocol-version))))
